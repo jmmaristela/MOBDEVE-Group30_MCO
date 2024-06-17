a798e7facae06162e813acf3e2b91f944fa7967c
Jm Maristela <joseph_maristela@dlsu.edu.ph>
2024-06-17 22:35:48 +0800

    Created initial project

---
+++ app/src/main/java/com/mobdeve/s12/mobdeve_maristela_ocampo_mco/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mobdeve.s12.mobdeve_maristela_ocampo_mco
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mobdeve.s12.mobdeve_maristela_ocampo_mco.ui.theme.MOBDEVE_Maristela_OcampoMCOTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MOBDEVE_Maristela_OcampoMCOTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MOBDEVE_Maristela_OcampoMCOTheme {
+        Greeting("Android")
+    }
+}
